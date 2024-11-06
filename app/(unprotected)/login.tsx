a7497de5732924c531952484bba5c7803fb5e696
Fortunenjima <njimafortune30@gmail.com>
2024-11-06 10:30:40 +0100

    e-commerce

---
+++ app/(unprotected)/login.tsx
@@ -0,0 +1,12 @@
+
+import Wrapper from '@/components/ui/Wrapper';
+import { Text, View} from 'react-native';
+
+export default function Login() {
+    return (
+       <Wrapper>
+        <Text>login</Text>
+       </Wrapper>
+    );
+}
+
