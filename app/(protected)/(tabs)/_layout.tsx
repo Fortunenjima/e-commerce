aa89a5f692ca1d645e5b028f96bfb69180fd525e
Fortunenjima <njimafortune30@gmail.com>
2024-11-11 13:07:24 +0100

    finished data

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,27 +1,41 @@
+import { CartIcon } from "@/components/Carticon";
 import Tabsicon from "@/components/Tabsicon";
 import { colors } from "@/constants";
 import { AntDesign } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function 
 TabsLayout() {
-return (<Tabs screenOptions={{
+return (
+    <>
+    <StatusBar barStyle='light-content'/>
+
+<Tabs screenOptions={{
     tabBarInactiveTintColor: colors.dark,
     tabBarActiveTintColor: colors.yellow,
+    headerTintColor: colors.yellow,
+    headerStyle: { backgroundColor: colors.dark },
+    headerRight: () => <CartIcon/>
 }}>
-        <Tabs.Screen name="index" options={{ title: "Home",
+        <Tabs.Screen name="index" options={{ title: "",
+        tabBarLabel: "Home",
             tabBarIcon: ({focused, size}) => <Tabsicon focused={focused} size={size} name="home"/> }} />
 
             
-        <Tabs.Screen name="menu" options={{ title: "Menu",
+        <Tabs.Screen name="menu" options={{ title: "",
+            tabBarLabel: "Menu",
             tabBarIcon: ({focused, size}) => <Tabsicon focused={focused} size={size} name="bars"/> }} />
 
             
-        <Tabs.Screen name="more" options={{ title: "More",
+        <Tabs.Screen name="more" options={{ title: "",
+            tabBarLabel: "More",
             tabBarIcon: ({focused, size}) => <Tabsicon focused={focused} size={size} name="amazon"/> }} />
 
             
-        <Tabs.Screen name="account" options={{ title: "Account",
+        <Tabs.Screen name="account" options={{ title: "",
+            tabBarLabel: "Account",
             tabBarIcon: ({focused, size}) => <Tabsicon focused={focused} size={size} name="user"/> }} />
-            </Tabs>)
-}
+            </Tabs>
+            </>);
+};
